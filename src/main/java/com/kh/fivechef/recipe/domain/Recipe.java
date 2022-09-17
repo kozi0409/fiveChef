f994499c934dd75b65d96fdd51a16af6e66d7902
DESKTOP-3CDT6EB\HMS <ka0ka0ka.ms@gmail.com>
2022-09-17 16:44:43 +0900

    Recipe class 수정

--- src/main/java/com/kh/fivechef/recipe/domain/Recipe.java
+++ src/main/java/com/kh/fivechef/recipe/domain/Recipe.java
@@ -1,8 +1,10 @@
 package com.kh.fivechef.recipe.domain;
 
+import java.sql.Date;
+
 public class Recipe {
 	private int recipeNo;
 	private String userId;
 	private String recipeTitle;
-	
+	private Date rCreateDate;
 }
