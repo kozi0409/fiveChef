8dd08f845f778563e7405dc72f8f589b1c054c86
DESKTOP-3CDT6EB\HMS <ka0ka0ka.ms@gmail.com>
2022-09-19 16:39:54 +0900

    home 수정용 푸시

--- src/main/java/com/kh/fivechef/recipe/domain/Ingradient.java
+++ src/main/java/com/kh/fivechef/recipe/domain/Ingradient.java
@@ -1,9 +1,9 @@
 package com.kh.fivechef.recipe.domain;
 
 public class Ingradient {
-//	private int ingNo;
-//	private int recipeNo;
-//	private String ingBundleName;
+	private int ingNo;
+	private int recipeNo;
+	private String ingBundleName;
 	private String ingAmount;
 	private String largeCatId;
 	private String smallCatId;
@@ -11,7 +11,26 @@ public class Ingradient {
 	
 	@Override
 	public String toString() {
-		return "Ingradient [ingAmount=" + ingAmount + ", largeCatId=" + largeCatId + ", smallCatId=" + smallCatId + "]";
+		return "Ingradient [ingNo=" + ingNo + ", recipeNo=" + recipeNo + ", ingBundleName=" + ingBundleName
+				+ ", ingAmount=" + ingAmount + ", largeCatId=" + largeCatId + ", smallCatId=" + smallCatId + "]";
+	}
+	public int getIngNo() {
+		return ingNo;
+	}
+	public void setIngNo(int ingNo) {
+		this.ingNo = ingNo;
+	}
+	public int getRecipeNo() {
+		return recipeNo;
+	}
+	public void setRecipeNo(int recipeNo) {
+		this.recipeNo = recipeNo;
+	}
+	public String getIngBundleName() {
+		return ingBundleName;
+	}
+	public void setIngBundleName(String ingBundleName) {
+		this.ingBundleName = ingBundleName;
 	}
 	public String getIngAmount() {
 		return ingAmount;
@@ -31,6 +50,7 @@ public class Ingradient {
 	public void setSmallCatId(String smallCatId) {
 		this.smallCatId = smallCatId;
 	}
+	
 	
 	
 	
