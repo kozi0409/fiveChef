403f24e4fa2ba14685d4b76db1a64c209d384139
gpwls <gpwls@DESKTOP-7VVNA2Q>
2022-09-19 14:51:32 +0900

    실험

--- src/main/java/com/kh/fivechef/notice/domain/Notice.java
+++ src/main/java/com/kh/fivechef/notice/domain/Notice.java
@@ -1,5 +1,95 @@
 package com.kh.fivechef.notice.domain;
 
-public class Notice {
+import java.util.Date;
 
+public class Notice {
+	private int noticeNo;
+	private int noticeCount;
+	private String noticeWriter;
+	private String noticeTitle;
+	private String noticeContents;
+	private String noticeFilepath;
+	private String noticeFilename;
+	private String noticeFileRename;
+	private String nStatus;
+	private Date nCreateDate;
+	private Date nUpdateDate;
+	
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+	public String getNoticeWriter() {
+		return noticeWriter;
+	}
+	public void setNoticeWriter(String noticeWriter) {
+		this.noticeWriter = noticeWriter;
+	}
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+	public String getNoticeContents() {
+		return noticeContents;
+	}
+	public void setNoticeContents(String noticeContents) {
+		this.noticeContents = noticeContents;
+	}
+	public String getNoticeFilepath() {
+		return noticeFilepath;
+	}
+	public void setNoticeFilepath(String noticeFilepath) {
+		this.noticeFilepath = noticeFilepath;
+	}
+	public String getNoticeFilename() {
+		return noticeFilename;
+	}
+	public void setNoticeFilename(String noticeFilename) {
+		this.noticeFilename = noticeFilename;
+	}
+	public String getNoticeFileRename() {
+		return noticeFileRename;
+	}
+	public void setNoticeFileRename(String noticeFileRename) {
+		this.noticeFileRename = noticeFileRename;
+	}
+	public int getNoticeCount() {
+		return noticeCount;
+	}
+	public void setNoticeCount(int noticeCount) {
+		this.noticeCount = noticeCount;
+	}
+	public Date getnCreateDate() {
+		return nCreateDate;
+	}
+	public void setnCreateDate(Date nCreateDate) {
+		this.nCreateDate = nCreateDate;
+	}
+	public Date getnUpdateDate() {
+		return nUpdateDate;
+	}
+	public void setnUpdateDate(Date nUpdateDate) {
+		this.nUpdateDate = nUpdateDate;
+	}
+	public String getnStatus() {
+		return nStatus;
+	}
+	public void setnStatus(String nStatus) {
+		this.nStatus = nStatus;
+	}
+	
+	@Override
+	public String toString() {
+		return "Notice [noticeNo=" + noticeNo + ", noticeWriter=" + noticeWriter + ", noticeTitle=" + noticeTitle
+				+ ", noticeContents=" + noticeContents + ", noticeFilepath=" + noticeFilepath + ", noticeFilename="
+				+ noticeFilename + ", noticeFileRename=" + noticeFileRename + ", noticeCount=" + noticeCount
+				+ ", nCreateDate=" + nCreateDate + ", nUpdateDate=" + nUpdateDate + ", nStatus=" + nStatus + "]";
+	}
+
+	
+	
 }
